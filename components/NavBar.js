76eb0e78cfce90bb537fb2f8db1ca588067265b2
lnuvy <hangeulgod@naver.com>
2022-01-12 12:34:09 +0900

    nav태그 클래스네임 삭제 / jsx 간단적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,19 +1,25 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   
   return (
-    <nav className={styles.nav}>
+    <nav>
       <Link href="/">
         <a>Home</a>
       </Link>
       <Link href="/about">
         <a>About</a>
       </Link>
-      
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+      `}</style>
     </nav>
   )
 }
