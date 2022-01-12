847ec9b586b685fa294a4d38e953e8fc349de6b5
lnuvy <hangeulgod@naver.com>
2022-01-13 01:13:37 +0900

    Loading

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Seo from "../components/Seo";
 const API_KEY = "71f43de951c136a669bee61f1fbf4c5b";
 
 export default function Home() {
-  const [moives, setMovies] = useState([]);
+  const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
       const { results } = await (
@@ -16,7 +16,8 @@ export default function Home() {
   return (
     <div>
       <Seo title="Home" />
-      {moives.map(movie => (
+      {!movies && <h4>Loading,,,</h4>} 
+      {movies?.map(movie => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
