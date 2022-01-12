1f102bd149ecad781bb9eb61180db26daeb97e96
lnuvy <hangeulgod@naver.com>
2022-01-13 03:42:13 +0900

    redirect 간단예시

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/contact/:path*",
+        destination: "/form/:path*",
+        permanent: false,
+      },
+    ];
+  },
 }
+
+// redirect 방식
