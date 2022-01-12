29737f16a5ecfe576cca7fed459d043940a50d51
lnuvy <hangeulgod@naver.com>
2022-01-13 01:11:04 +0900

    api키로 제목 map 하기

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "71f43de951c136a669bee61f1fbf4c5b";
+
 export default function Home() {
+  const [moives, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      ).json();
+      setMovies(results);
+    })(); // () 확인
+  }, [])
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Hello</h1>
+      {moives.map(movie => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
